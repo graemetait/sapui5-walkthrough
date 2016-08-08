c89d0cec78b8f2523062c5f5dcc5a49533f5de2b
Graeme Tait <burriko@gmail.com>
2016-08-08 10:25:32 +0100

    Step 19.1

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,9 +1,8 @@
 sap.ui.define([
   "sap/ui/core/UIComponent",
   "sap/ui/model/json/JSONModel",
-  "sap/ui/model/resource/ResourceModel",
   "sap/ui/demo/wt/controller/HelloDialog"
-], function (UIComponent, JSONModel, ResourceModel, HelloDialog) {
+], function (UIComponent, JSONModel, HelloDialog) {
   "use strict";
   return UIComponent.extend("sap.ui.demo.wt.Component", {
     metadata: {
@@ -22,11 +21,6 @@ sap.ui.define([
       this.setModel(oModel);
       // set dialog
       this.helloDialog = new HelloDialog();
-
-      var i18nModel = new ResourceModel({
-        bundleName: "sap.ui.demo.wt.i18n.i18n"
-      });
-      this.setModel(i18nModel, "i18n");
     }
   });
 });
